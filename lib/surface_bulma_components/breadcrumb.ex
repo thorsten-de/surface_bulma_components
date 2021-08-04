4c850c36cba2e6274373b6379b86756fd5168252
Thorsten Deinert <thorsten.deinert@udo.edu>
2021-08-04 11:41:11 +0200

    simple bulma breadcrumb component added

---
+++ lib/surface_bulma_components/breadcrumb.ex
@@ -0,0 +1,54 @@
+defmodule SurfaceBulmaComponents.Breadcrumb do
+  @moduledoc """
+  Simple navigation component
+  """
+  use Surface.Component
+  alias Surface.Components.Link
+
+  @doc """
+  List of breadcrumbs to display. Can be a list of labels or a list of maps
+  """
+  prop breadcrumbs, :list, required: true
+
+  @doc """
+  CSS classes that are appended to standard "breadcrumb" class
+  """
+  prop class, :css_class
+
+  @doc """
+  Template to be rendered inside the li for each breadcrumb defined in breadcrumbs
+  """
+  slot default, args: [:item]
+
+  def update(%{breadcrumbs: breadcrumbs} = assigns, socket) do
+    breadcrumbs =
+      breadcrumbs
+      |> Enum.map(fn
+        crumb when is_map(crumb) -> IO.inspect(crumb)
+        label when is_binary(label) -> %{label: label, to: "#"}
+      end)
+
+    socket =
+      socket
+      |> assign(assigns)
+      |> assign(:breadcrumbs, breadcrumbs)
+
+    {:ok, socket}
+  end
+
+  def render(assigns) do
+    ~F"""
+      <nav class={"breadcrumb", @class} aria-label="breadcrumbs">
+        <ul>
+          {#for b <- @breadcrumbs}
+            <li class={"is-active": b[:is_active]} title={b[:title]}>
+              <#slot :args={item: b}>
+                <Link to={b[:to]} label={b[:label]} />
+              </#slot>
+            </li>
+          {/for}
+        </ul>
+      </nav>
+    """
+  end
+end
